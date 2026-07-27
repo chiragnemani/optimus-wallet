a0f9e846f036220f21291cdfec3bd963e2052922
Chirag Nemani <148119942+chiragnemani@users.noreply.github.com>
2026-07-26 21:17:53 -0400

    Update service worker cache management
    Updated cache name and added cache management for old caches.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'optimus-v1';
+const CACHE_NAME = 'optimus-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -9,12 +9,30 @@ const ASSETS = [
   './icon-512.png'
 ];
 
+// Install new cache
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Clear old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Serve cached files
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
